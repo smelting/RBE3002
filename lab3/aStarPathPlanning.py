3a72a56f64a9e82b595f694181b8dbd3077b21fc
Nick Sorensen <smeltingpot.tutorials@gmail.com>
2017-04-05 16:39:50 -0400

    A* at least runs.....

--- lab3/aStarPathPlanning.py
+++ lab3/aStarPathPlanning.py
@@ -1,29 +1,32 @@
 
 import math, rospy
-from geometry_msgs import Point
+from Queue import PriorityQueue
+from geometry_msgs.msg import Point
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from geometry_msgs.msg import Pose
 from nav_msgs.msg import MapMetaData
 from nav_msgs.msg import OccupancyGrid
+from collections import defaultdict
 
 
 class Node:
 
 	def __init__(self, x, y):
-	self.x = x
-	self.y = y
+		self.x = x
+		self.y = y
+		self.parent = (0,0)
+		self.cost = 0
 
-	def inBounds(self, pos):
+	def inBounds(self, node):
 		global width
 		global height
-		(x,y) = pos
-		return 0 <= x < width and 0 <= y < height
+		(x,y) = node
+		return 0 <= x < width*res and 0 <= y < height*res
 
-	def neighbors(self, pos):
-		(x,y) = pos
-		neighbors = [(x+1, y), (x, y-1), (x-1, y), (x,y+1)]
-		neighbors = filter(self.in_bounds, neighbors)
+	def neighbors(self):
+		neighbors = [(self.x+res, self.y), (self.x, self.y-res), (self.x-res, self.y), (self.x,self.y+res)]
+		neighbors = filter(self.inBounds, neighbors)
 		return neighbors
 
 
@@ -32,16 +35,19 @@ def startPoseCallback(msg):
 	startPose = Pose()
 	startPose = msg.pose.pose
 	setStart = True
+	print "starting at x %f y %f" %(startPose.position.x,startPose.position.y)
 
 def goalPoseCallback(msg):
+	global newGoal
 	global goalPose
 	gaolPose = Pose()
 	goalPose = msg.pose
 	newGoal = True
+	print "ending at x %f y %f" %(goalPose.position.x,goalPose.position.y)
 
 def heuristic(a, b):
-	(x1, y1) = a
-	(x2, y2) = b
+	(x1,y1) = a
+	(x2,y2) = b
 	return abs(x1 - x2) + abs(y1 - y2)
 
 def matchGridPose(p):
@@ -50,52 +56,84 @@ def matchGridPose(p):
 	(x,y) = (round(x/res), round(y/res))
 	return (x*res, y*res)
 
+def matchPoseIndex(p):
+	(x,y) = p
+	ticks = x/res + (y/res*width)
+	return int(ticks)
+
 
 def mapCallBack(msg):
+	print "grabbing map"
 	global grid
 	global width
 	global height
 	global res
+	global gridOrigin
 	grid = []
 	width = msg.info.width
 	height = msg.info.height
 	res = msg.info.resolution
+	print "grid res is %f m" % res
+	print "width %f height %f" % (width,height)
 	countCol = 1
 	countRow = 1
-	startX = msg.info.pose.x
-	startY = msg.info.pose.y
+	startX = msg.info.origin.position.x
+	startY = msg.info.origin.position.y
+	gridOrigin = msg.info.origin
+	print "0,0   %f  %f " % (startX,startY)
+	count = 0
 	for new in msg.data:
-		if(countCol = width): #hit the end of the column
+		x = (countCol + startX - 1)*res
+		y = (countRow + startY - 1)*res
+		if(countCol == width): #hit the end of the column
 			countRow = countRow + 1
 			countCol = 0
-		if(countRow <= height):
-			grid.append(Node(countCol + startX - 1, countRow + startY - 1))
+		if(countRow <= height and new != 100):
+			if(count < 10):
+				print "Making index %f x:%f y:%f with inten %f" %(count,x,y,new)
+			grid.append(Node(x,y))
+			count += 1
+			countCol += 1
 
+
+def genPath(start,goal):
+	path = []
+	current = goal
+	while current != start:
+		path.append(current)
+		current = grid[matchPoseIndex(current)].parent
+		print "next x %f y %f" % (current)
+		rospy.sleep(.5)
 
 def a_star_search(start, goal):
+
 	global newGoal
+	goal = matchGridPose(goal)
+	start = matchGridPose(start)
 	frontier = PriorityQueue()
-	frontier.put(start, 0)
-	came_from = {}
-	cost_so_far = {}
-	came_from[start] = None
-	cost_so_far[start] = 0
-	
+	print "i: %f %f %f" % (matchPoseIndex(start),grid[matchPoseIndex(start)].x,grid[matchPoseIndex(start)].y)
+	frontier.put(grid[matchPoseIndex(start)], 0)
+	visited = []
+	print "starting A* at x %f y %f" % (start)
 	while not frontier.empty():
 		current = frontier.get()
-		
-		if current == goal:
+		print "current: x %f y %f" %(current.x,current.y)
+		if(current.x == goal[0] and current.y == goal[1]):
 			break
-		
-		for next in grid.neighbors(current):
-			new_cost = cost_so_far[current] + graph.cost(current, next)
-			if next not in cost_so_far or new_cost < cost_so_far[next]:
-				cost_so_far[next] = new_cost
-				priority = new_cost + heuristic(goal, next)
-				frontier.put(next, priority)
-				came_from[next] = current
+
+		for next in current.neighbors():
+			newCost = current.cost + 1
+			if grid[matchPoseIndex(next)] not in visited or newCost < grid[matchPoseIndex(next)].cost:
+				grid[matchPoseIndex(next)].cost = newCost
+				priority = newCost + heuristic(goal, start)
+				frontier.put(grid[matchPoseIndex(next)],priority)
+				grid[matchPoseIndex(next)].parent = (current.x,current.y)
+				visited.append(grid[matchPoseIndex(next)])
+				#print "LOLOL"
+				#rospy.sleep(0.01)
 	newGoal = False
-	return came_from, cost_so_far
+	print "done with A*"
+	return genPath(start,goal)
 
 
 if __name__ == '__main__':
@@ -105,11 +143,12 @@ if __name__ == '__main__':
 	setStart = False
 	newGoal = False
 
-	rospy.Subscriber("/move_base_simple/goal",PoseStamped, goalPoseCallback, queuesize = 1)
-	rospy.Subscriber("/initialpose",PoseWithCovarianceStamped, startPoseCallback, queuesize = 1)
-	rospy.Subscriber("/map", OccupancyGrid, mapCallBack, queuesize = 1)
+	rospy.Subscriber("/move_base_simple/goal",PoseStamped, goalPoseCallback, queue_size = 1)
+	rospy.Subscriber("/initialpose",PoseWithCovarianceStamped, startPoseCallback, queue_size = 1)
+	rospy.Subscriber("/map", OccupancyGrid, mapCallBack, queue_size = 1)
 
 	while(1):
 		rospy.sleep(0.1)
 		if(newGoal):
-			a_star_search(startPose,goalPose)
+			a_star_search((startPose.position.x,startPose.position.y),(goalPose.position.x,goalPose.position.y))
+			newGoal = False
